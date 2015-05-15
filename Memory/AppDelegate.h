f7a78ccc1e12ca4032aa8e63ea611368bb8af4e4
yuki66 <nktiga6@gmail.com>
2015-05-15 21:16:13 +0900

    Initial Commit

---
+++ Memory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memory
+//
+//  Created by YUKIKO HARADA on 2015/05/15.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
