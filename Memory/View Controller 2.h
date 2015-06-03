3b3647ae1e4b29405dcd665e4433afa5ce5a3685
yuki66 <nktiga6@gmail.com>
2015-06-03 22:55:24 +0900

    UITouch started

---
+++ Memory/View Controller 2.h
@@ -0,0 +1,23 @@
+//
+//  View Controller 2.h
+//  Memory
+//
+//  Created by YUKIKO HARADA on 2015/06/03.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#ifndef Memory_View_Controller_2_h
+#define Memory_View_Controller_2_h
+#import <UIKit/UIKit.h>
+@interface #import <UIKit/UIKit.h>
+@interface View Controller 2:UIViewController
+<UI   Delegate>
+{
+    IBOutlet UITouch *touch;
+}
+
+
+@end
+
+
+#endif
---
+++ Memory/View Controller 2.m
@@ -0,0 +1,12 @@
+//
+//  View Controller 2.m
+//  Memory
+//
+//  Created by YUKIKO HARADA on 2015/06/03.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+}
