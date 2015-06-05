9992d484c6a3c7abac91ed5581cb5b7d014f9cf1
yuki66 <nktiga6@gmail.com>
2015-06-05 22:30:08 +0900

    draw line starting

---
+++ Memory/WorkViewController.h
@@ -0,0 +1,16 @@
+//
+//  WorkViewController.h
+//  Memory
+//
+//  Created by YUKIKO HARADA on 2015/06/05.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *canvas;
+
+
+@end
---
+++ Memory/WorkViewController.m
@@ -0,0 +1,112 @@
+//
+//  WorkViewController.m
+//  Memory
+//
+//  Created by YUKIKO HARADA on 2015/06/05.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import "WorkViewController.h"
+
+@interface WorkViewController ()
+{
+    UIBezierPath *bezierPath;
+    UIImage *lastDrawImage;
+    NSMutableArray *undoStack;
+    NSMutableArray *redoStack;
+}
+@end
+
+@implementation WorkViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    undoStack = [NSMutableArray array];
+    redoStack = [NSMutableArray array];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // タッチした座標を取得します。
+    CGPoint currentPoint = [[touches anyObject] locationInView:self.canvas];
+    
+          return;
+    }
+    
+    // パスを初期化します。
+    bezierPath = [UIBezierPath bezierPath];
+    bezierPath.lineCapStyle = kCGLineCapRound;
+    bezierPath.lineWidth = 4.0;
+    [bezierPath moveToPoint:currentPoint];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // タッチ開始時にパスを初期化していない場合は処理を終了します。
+    if (bezierPath == nil){
+        return;
+    }
+    
+    // タッチした座標を取得します。
+    CGPoint currentPoint = [[touches anyObject] locationInView:self.canvas];
+    
+    // パスにポイントを追加します。
+    [bezierPath addLineToPoint:currentPoint];
+    
+    // 線を描画します。
+    [self drawLine:bezierPath];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // タッチ開始時にパスを初期化していない場合は処理を終了します。
+    if (bezierPath == nil){
+        return;
+    }
+    
+    // タッチした座標を取得します。
+    CGPoint currentPoint = [[touches anyObject] locationInView:self.canvas];
+    
+    // パスにポイントを追加します。
+    [bezierPath addLineToPoint:currentPoint];
+    
+    // 線を描画します。
+    [self drawLine:bezierPath];
+    
+    // 今回描画した画像を保持します。
+    lastDrawImage = self.canvas.image;
+    
+    // undo用にパスを保持して、redoスタックをクリアします。
+    [undoStack addObject:bezierPath];
+    [redoStack removeAllObjects];
+    bezierPath = nil;
+    
+}
+
+- (void)drawLine:(UIBezierPath*)path
+{
+    // 非表示の描画領域を生成します。
+    UIGraphicsBeginImageContext(self.canvas.frame.size);
+    
+    // 描画領域に、前回までに描画した画像を、描画します。
+    [lastDrawImage drawAtPoint:CGPointZero];
+    
+    // 色をセットします。
+    [[UIColor blackColor] setStroke];
+    
+    // 線を引きます。
+    [path stroke];
+    
+    // 描画した画像をcanvasにセットして、画面に表示します。
+    self.canvas.image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    // 描画を終了します。
+    UIGraphicsEndImageContext();
+}@end
